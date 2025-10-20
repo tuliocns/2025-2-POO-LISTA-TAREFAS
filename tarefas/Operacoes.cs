ab86b7f64f6fc2db4e1cb8d441d63ce9ef9cb9d6
Túlio <163613757+tuliocns@users.noreply.github.com>
2025-10-20 23:40:30 +0000

    2025-2-POO-LISTA-TAREFAS

--- tarefas/Operacoes.cs
+++ tarefas/Operacoes.cs
@@ -1,27 +1,28 @@
+using MySql.Data.MySqlClient;
+
 public class Operacoes
 {
-    private string connectionString = "";
+    private string connectionString = 
+    @"server=phpmyadmin.uni9.marize.us;User ID=user_poo;password=S3nh4!F0rt3;database=user_poo;";
     public int Criar(Tarefa tarefa)
     {
         using(var conexao = new MySqlConnection(connectionString))
         {
             conexao.Open();
-            string sql = @"INSERT INTO tarefa (nome, descricao, dataCriacao, status, dataExecucao)
-                            VALUES (@nome, @descricao, @dataCriacao, @status, @dataExecucao);
-                            SELECT LAST_INSERT_ID();";
+            string sql = @"INSERT INTO tarefa (nome, descricao, dataCriacao, status, dataExecucao) 
+                           VALUES (@nome, @descricao, @dataCriacao, @status, @dataExecucao);
+                           SELECT LAST_INSERT_ID();";
             using (var cmd = new MySqlCommand(sql, conexao))
             {
-                cmd.Parameters.AddWithValue("nome", tarefa.Nome);
-                cmd.Parameters.AddWithValue("descricao", tarefa.Descricao);
-                cmd.Parameters.AddWithValue("dataDescricao", tarefa.NdataDescricao);
-                cmd.Parameters.AddWithValue("status", tarefa.status;
-                cmd.Parameters.AddWithValue("dataExecucao", tarefa.dataExecucao);
+                cmd.Parameters.AddWithValue("@nome", tarefa.Nome);
+                cmd.Parameters.AddWithValue("@descricao", tarefa.Descricao);
+                cmd.Parameters.AddWithValue("@dataCriacao", tarefa.DataCriacao);
+                cmd.Parameters.AddWithValue("@status", tarefa.Status);
+                cmd.Parameters.AddWithValue("@dataExecucao", tarefa.DataExecucao);
 
-                return convert.ToInt32(cmd.ExecuteScalar());
-            
+                return Convert.ToInt32(cmd.ExecuteScalar());
             }
         }
-        return 0;
     }
 
     public Tarefa Buscar(int id)
@@ -29,14 +30,14 @@ public class Operacoes
         return null;
     }
 
-    public List<Tarefa> Listar()
+    public IList<Tarefa> Listar()
     {
         return Array.Empty<Tarefa>();
     }
 
     public void Alterar(Tarefa tarefa)
     {
-    
+
     }
 
     public void Excluir(int id)
