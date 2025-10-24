39ae3fc102fb575d0eccde0923f780d00165cc07
Túlio <163613757+tuliocns@users.noreply.github.com>
2025-10-24 00:49:05 +0000

    2025-2-POO-LISTA-TAREFAS

--- tarefas/Operacoes.cs
+++ tarefas/Operacoes.cs
@@ -64,13 +64,41 @@ public class Operacoes
         return tarefas;
     }
 
-    public void Alterar(Tarefa tarefa)
+  public void Alterar(Tarefa tarefa)
+{
+    using (var conexao = new MySqlConnection(connectionString))
     {
+        conexao.Open();
+        string sql = @"UPDATE tarefa 
+                       SET nome = @nome, descricao = @descricao, dataCriacao = @dataCriacao,
+                           status = @status, dataExecucao = @dataExecucao
+                       WHERE id = @id;";
 
+        using (var cmd = new MySqlCommand(sql, conexao))
+        {
+            cmd.Parameters.AddWithValue("@nome", tarefa.Nome);
+            cmd.Parameters.AddWithValue("@descricao", tarefa.Descricao);
+            cmd.Parameters.AddWithValue("@dataCriacao", tarefa.DataCriacao);
+            cmd.Parameters.AddWithValue("@status", tarefa.Status);
+            cmd.Parameters.AddWithValue("@dataExecucao", tarefa.DataExecucao);
+            cmd.Parameters.AddWithValue("@id", tarefa.Id);
+
+            cmd.ExecuteNonQuery();
+        }
     }
+}
 
-    public void Excluir(int id)
+public void Excluir(int id)
+{
+    using (var conexao = new MySqlConnection(connectionString))
     {
+        conexao.Open();
+        string sql = "DELETE FROM tarefa WHERE id = @id;";
 
+        using (var cmd = new MySqlCommand(sql, conexao))
+        {
+            cmd.Parameters.AddWithValue("@id", id);
+            cmd.ExecuteNonQuery();
+        }
     }
 }
